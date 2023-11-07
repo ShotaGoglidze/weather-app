924717e0e37b21140e7e0b69fa241087ba5a24b0
Shota Goglidze <sg@gmail.com>
2023-11-07 04:47:38 -0800

    add searchBox functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const apiKey = '2e0486f14dcd36830da7df1bfe1c5f20&units';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=tbilisi';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
 
-async function checkWeather (){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather (city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
@@ -13,4 +15,7 @@ async function checkWeather (){
     document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 }
 
-checkWeather();
+searchBtn.addEventListener('click', ()=> {
+    checkWeather(searchBox.value);
+})
+
